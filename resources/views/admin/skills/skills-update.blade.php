3a6be046ab01181ffcead888cc0593b92aad5560
Ahmed Elmsery <91689994+AhmedElMseryy@users.noreply.github.com>
2024-12-03 16:24:33 +0200

    enhancing edit modal

--- resources/views/admin/skills/skills-update.blade.php
+++ resources/views/admin/skills/skills-update.blade.php
@@ -1,40 +1,16 @@
-<!-- Modal -->
-<div class="modal fade" id="editModal" tabindex="-1" aria-hidden="true" wire:ignore.self>
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="exampleModalLabel1">Edit Skill</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form wire:submit.prevent='submit'>
-                <div class="modal-body">
-                    <div class="row">
-                        <!-- NAME -->
-                        <div class="col-md-6 mb-0">
-                            <label class="form-label">Name</label>
-                            <input type="text" class="form-control" placeholder="Name" wire:model='name' />
-                            <x-error-message field="name"></x-error-message>
-                        </div>
-
-                        <!-- PROGRESS -->
-                        <div class="col-md-6 mb-0">
-                            <label class="form-label">Progress</label>
-                            <input type="number" class="form-control" placeholder="10" min="1" max="100"
-                                wire:model='progress' />
-                            <x-error-message field="progress"></x-error-message>
-                        </div>
-                    </div>
-                </div>
-
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-                        Close
-                    </button>
-                    <button type="submit" class="btn btn-primary">
-                        <x-loading-state name="Update"></x-loading-state>
-                    </button>
-                </div>
-            </form>
-        </div>
+<x-edit-modal title="Edit Skill">
+    <!-- NAME -->
+    <div class="col-md-6 mb-0">
+        <label class="form-label">Name</label>
+        <input type="text" class="form-control" placeholder="Name" wire:model='name' />
+        <x-error-message field="name"></x-error-message>
     </div>
-</div>
+
+    <!-- PROGRESS -->
+    <div class="col-md-6 mb-0">
+        <label class="form-label">Progress</label>
+        <input type="number" class="form-control" placeholder="10" min="1" max="100"
+            wire:model='progress' />
+        <x-error-message field="progress"></x-error-message>
+    </div>
+</x-edit-modal>
