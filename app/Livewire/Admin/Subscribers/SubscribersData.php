ab9fa0459912b6870fac984f487a5273b1cb0b30
Ahmed Elmsery <91689994+AhmedElMseryy@users.noreply.github.com>
2024-12-03 19:30:05 +0200

    subscribers data component

---
+++ app/Livewire/Admin/Subscribers/SubscribersData.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Livewire\Admin\Subscribers;
+
+use App\Models\Subscriber;
+use Livewire\Component;
+use Livewire\WithPagination;
+
+class SubscribersData extends Component
+{
+    use WithPagination;
+
+    public $search;
+
+    protected $listeners = ['refreshData' => '$refresh'];
+
+    public function updatingSearch()
+    {
+        $this->resetPage();
+    }
+
+    public function render()
+    {
+        $data = Subscriber::where('email', 'like', '%' . $this->search . '%')->paginate(1);
+        return view('admin.subscribers.subscribers-data', get_defined_vars());
+    }
+}
--- resources/views/admin/subscribers/index.blade.php
+++ resources/views/admin/subscribers/index.blade.php
@@ -9,18 +9,13 @@
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="mb-3">
             <h4 class="fw-bold py-3 mb-4 d-inline"> Subscribers</h4>
-
-            <!--******************* Skills Create *******************-->
-            {{-- @livewire('admin.skills.skills-create') --}}
-            <!--***************************************************-->
-
         </div>
 
         <!-- Table -->
         <div class="card mb-4">
             <div class="card-body">
                 <!--******************* Skills Data *******************-->
-                {{-- @livewire('admin.skills.skills-data') --}}
+                @livewire('admin.subscribers.subscribers-data')
                 <!--***************************************************-->
             </div>
             <!--************ Skills Update & Delete & Show ************-->
