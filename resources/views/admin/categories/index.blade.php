37fa41a70ea35b2166e7e8c1975463f9ce78d944
Ahmed Elmsery <91689994+AhmedElMseryy@users.noreply.github.com>
2024-12-12 18:10:16 +0200

    categories data component

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -9,18 +9,26 @@
     <div class="container-xxl flex-grow-1 container-p-y">
         <div class="mb-3">
             <h4 class="fw-bold py-3 mb-4 d-inline"> Categories</h4>
+            <button type="button" class="btn btn-sm btn-primary mb-2 mx-2" data-bs-toggle="modal"
+                data-bs-target="#createModal">
+                Add New
+            </button>
+
+            <!--******************* categories Create *******************-->
+            {{-- @livewire('admin.categories.categories-create') --}}
+            <!--***************************************************-->
         </div>
 
         <!-- Table -->
         <div class="card mb-4">
             <div class="card-body">
-                <!--******************* Skills Data *****************-->
-                {{-- @livewire('admin.categories.categories-data') --}}
+                <!--******************* categories Data *****************-->
+                @livewire('admin.categories.categories-data')
                 <!--*************************************************-->
             </div>
-            <!--******************* Delete & Show *******************-->
+            <!--******************* Delete & edit *******************-->
+            {{-- @livewire('admin.categories.categories-edit') --}}
             {{-- @livewire('admin.categories.categories-delete') --}}
-            {{-- @livewire('admin.categories.categories-show') --}}
             <!--*****************************************************-->
         </div>
 
